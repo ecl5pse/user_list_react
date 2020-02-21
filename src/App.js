654662af2891c2c98a00a2a58d3fbd01314c7502
ecl5pse <fantamas20@gmail.com>
2020-02-21 20:48:59 +0200

    best commit :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component , Fragment} from 'react';
+import  styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = [
+      {
+        id: 1,
+        firstName: 'Brian',
+        lastName: 'McCauley',
+        profileImage: 'https://marvel-live.freetls.fastly.net/canvas/2018/7/b637481eb0374435843150483fbb5fe8?quality=95&fake=.png',
+        isCheck:true
+
+      },
+      {
+        id: 2,
+        firstName: 'Chad',
+        lastName: 'Hayes',
+        profileImage: 'https://marvel-live.freetls.fastly.net/canvas/2018/7/b637481eb0374435843150483fbb5fe8?quality=95&fake=.png',
+        isCheck:true
+      },
+
+      {
+        id: 3,
+        firstName: 'Jeff',
+        lastName: 'Buck',
+        profileImage: 'https://marvel-live.freetls.fastly.net/canvas/2018/7/b637481eb0374435843150483fbb5fe8?quality=95&fake=.png',
+        isCheck:true
+      },
+      {
+        id: 4,
+        firstName: 'Kenny',
+        lastName: 'Griffin',
+        profileImage: 'https://marvel-live.freetls.fastly.net/canvas/2018/7/b637481eb0374435843150483fbb5fe8?quality=95&fake=.png',
+        isCheck:true
+      },
+
+      {
+        id: 5,
+        firstName: 'Tom',
+        lastName: 'Glover',
+        profileImage: 'https://marvel-live.freetls.fastly.net/canvas/2018/7/b637481eb0374435843150483fbb5fe8?quality=95&fake=.png',
+        isCheck:true
+      }
+
+    ]
+  }
+
+
+
+
+
+
+  render() {
+    return (
+        <Fragment>
+          <div className={styles.header}></div>
+          {
+            this.state.map((state) => (
+                <div className={styles.container} key={state.id}>
+                  <h1 className={styles.name}> {state.firstName}{state.lastName}</h1>
+                  <h4 className={styles.level}> Level{state.id}</h4>
+                  <img src={state.profileImage} alt='profile image'/>
+                  <div className={styles.checkbox}></div>
+                </div>
+            ))
+
+          }
+        </Fragment>
+    )
+  }
 }
+
 
 export default App;
